71b41eaf93d13d112a31e346792b63f123052c15
Chaplin16 <chaplin16@orange.fr>
2021-09-09 17:29:45 +0200

    premier carrousel maillot de bain femme

---
+++ js/index.js
@@ -0,0 +1,54 @@
+function carousel(selector, data) {
+    let element = document.querySelector(selector);
+    data.forEach(function(d) {
+        element.innerHTML += 
+        `<div class="card-img"><img src="${d.attachment}" alt="${d.alt}" class=""></div>`;
+    });
+    $(element).slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 1,
+        arrows: true,
+        adaptiveHeight: true,
+        responsive: [{
+                breakpoint: 1300,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+        //    },
+        //     {
+        //         breakpoint: 1000,
+        //         settings: {
+        //             arrows: true,
+        //             centerMode: true,
+        //             centerPadding: '40px',
+        //             slidesToShow: 2.5
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //             arrows: true,
+        //             centerMode: true,
+        //             centerPadding: '40px',
+        //             slidesToShow: 1.5
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             arrows: true,
+        //             centerMode: true,
+        //             centerPadding: '40px',
+        //             slidesToShow: 1
+        //         }
+             }
+         ]
+    });
+}
+
+carousel(".firstCarousel", swimsuit);
+
---
+++ js/swimsuit.js
@@ -0,0 +1,67 @@
+const swimsuit = [
+    {
+        id:1,
+        attachment: "images/bikini.jpeg",
+        alt:" Plusieurs maillots de bain colorés à la boutique Nuit de Folie à Aigues-Mortes"
+    },
+    {
+        id:2,
+        attachment: "images/one-piece-swimsuit-3.jpeg",
+        alt:" Maillots de bain chez Nuit de Folie à Aigues-Mortes"
+    },
+    {
+        id:3,
+        attachment: "images/one-piece-swimsuit-4.jpeg",
+        alt:" Maillots de bain multicolores deux pieces à la boutique Nuit de Folie"
+    },
+    {
+        id:4,
+        attachment: "images/one-piece-swimsuit-2.jpeg",
+        alt:" Maillots de bain blancs et noirs dans le magasin NUIT DE FOLIE"
+    },
+    {
+        id:5,
+        attachment: "images/dummy-swimsuit-beige.jpeg",
+        alt:"Mannequin portant un deux pieces beige doré à la boutique Nuit de Folie"
+    },
+    {
+        id:6,
+        attachment: "images/dummy-swimsuit.jpeg",
+        alt:"Mannequin portant un deux pieces vert kaki à la boutique d'Aigues-Mortes"
+    },
+    {
+        id:7,
+        attachment: "images/swimsuit-fuchsia.jpeg",
+        alt:"soutien-gorge du maillot de bain à fleur à la boutique Nuit de Folie"
+    },
+    {
+        id:8,
+        attachment: "images/swimsuit-sailor.jpeg",
+        alt:"soutien-gorge du maillot de bain imprimé 'marin' à la boutique Nuit de Folie d'Aigues-Mortes"
+    },
+    {
+        id:9,
+        attachment: "images/swimsuit-multicoloured.jpeg",
+        alt:"soutien-gorge du maillot de bain imprimé multicolor à la boutique Nuit de Folie d'Aigues-Mortes"
+    },
+    {
+        id:10,
+        attachment: "images/swimsuit-pink.jpeg",
+        alt:"Soutien-gorge du maillot de bain rose chez Nuit de Folie"
+    },
+    {
+        id:11,
+        attachment:"images/swimsuit-multicoloured-light.jpeg",
+        alt:"Soutien-gorge du maillot de bain multicolor clair chez Nuit de Folie"
+    },
+    {
+        id:12,
+        attachment: "images/swimsuit-black.jpeg",
+        alt:"soutien-gorge du maillot de bain noir chez Nuit de Folie"
+    },
+    {
+        id:13,
+        attachment: "images/one-piece-swimsuit.jpeg",
+        alt:"Maillot de bain une pièce à la boutique Nuit de Folie"
+    }
+]
